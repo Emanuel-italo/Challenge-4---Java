2ed61561069b77ada2de92e05988f556fe016a1c
emanuel-italo <emanuelitaloleal@hotmail.com>
2025-11-09 19:17:32 -0300

    ajustando apikey

--- src/main/java/br/com/fiap/saudetodos/infrastructure/security/ApiKeyFilter.java
+++ src/main/java/br/com/fiap/saudetodos/infrastructure/security/ApiKeyFilter.java
@@ -1,48 +1,40 @@
 package br.com.fiap.saudetodos.infrastructure.security;
-
 import br.com.fiap.saudetodos.domain.service.ApiKeyValidator;
-
-import jakarta.annotation.Priority;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.NotAuthorizedException;
-import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 
 import java.io.IOException;
 
 
 @Provider
-@Priority(Priorities.AUTHENTICATION)
 public class ApiKeyFilter implements ContainerRequestFilter {
 
-
-    private static final String API_KEY_HEADER = "X-API-Key";
-
-
-    private final ApiKeyValidator apiKeyValidator;
-
     @Inject
-    public ApiKeyFilter(ApiKeyValidator apiKeyValidator) {
-
-        this.apiKeyValidator = apiKeyValidator;
-    }
+    ApiKeyValidator apiKeyValidator;
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
 
-        final String apiKey = requestContext.getHeaderString(API_KEY_HEADER);
-
-
-        if (!apiKeyValidator.isValid(apiKey)) {
-
-            System.err.println("BLOQUEADO: Requisição sem API Key válida para " + requestContext.getUriInfo().getPath());
-
-            throw new NotAuthorizedException("Chave de API (X-API-Key) inválida ou ausente.");
+       
+        if (requestContext.getUriInfo().getPath().contains("swagger") || requestContext.getUriInfo().getPath().contains("q/swagger-ui")) {
+            return;
         }
 
+        
+        if (requestContext.getMethod().equalsIgnoreCase("OPTIONS")) {
+            return;
+        }
 
-        System.out.println("PERMITIDO: API Key válida recebida para " + requestContext.getUriInfo().getPath());
+        
+        String apiKey = requestContext.getHeaderString("X-API-KEY");
+        if (apiKey == null || !apiKeyValidator.isValid(apiKey)) {
+            requestContext.abortWith(
+                Response.status(Response.Status.UNAUTHORIZED)
+                        .entity("API Key inválida ou ausente.")
+                        .build());
+        }
     }
 }
